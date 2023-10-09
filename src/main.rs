a9ff5a0ba7991c68ac9a111deec1e55cc208ec9b
JoshuaMontreuil <98335422+JoshuaMontreuil@users.noreply.github.com>
2023-10-09 14:13:05 +0200

    Création de fichiers pour la séparations en modules.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod display;
+mod utils;
 
 extern crate rand;
 
@@ -9,7 +10,7 @@ use rand::Rng;
 //use std::time::{Instant, Duration};
 //use std::thread;
 
-const BOARD_SIZE: usize = 5;
+const BOARD_SIZE: usize = 8;
 const NUM_MINES: usize = 10;
 //const FIRST_PLAY: bool = false;
 
