2f784a68711d59f71121e697c06c4f2534a321c1
Théo Benard <theo.benard@reseau.eseo.fr>
2023-10-20 13:47:00 +0200

    Suppression des commentaires dans le fichier main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,3 @@ mod game;
 fn main() {
     game::main_game();
 }
-
-/*
-    TIMER à ajouter par la suite
-
-    let mut start_time = Instant::now();
-
-    loop {
-        let elapsed_time = start_time.elapsed();
-        let seconds = elapsed_time.as_secs();
-        let minutes = seconds / 60;
-        let remaining_seconds = seconds % 60;
-
-        // Effacez l'écran (pour éviter que le temps précédent ne soit affiché)
-        print!("\x1B[2J\x1B[1;1H");
-
-        // Affichez le temps écoulé au format mm:ss
-        println!("{:02}:{:02}", minutes, remaining_seconds);
-
-        // Attendez une seconde avant la prochaine mise à jour
-        thread::sleep(Duration::from_secs(1));
-    }
-    
-*/
